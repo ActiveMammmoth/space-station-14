982f6397f9f39cf0412782187593b2afc2bf83ee
ActiveMammmoth <kmcsmooth@gmail.com>
2025-02-13 13:06:21 -0500

    reworking petrify into polymorph in different branch

--- Content.Shared/Magic/Systems/PetrificationSystem.cs
+++ Content.Shared/Magic/Systems/PetrificationSystem.cs
@@ -1,6 +1,8 @@
 using Content.Shared.Emoting;
+using Content.Shared.Eye.Blinding.Systems;
 using Content.Shared.Hands;
 using Content.Shared.Interaction.Events;
+using Content.Shared.Inventory.Events;
 using Content.Shared.Item;
 using Content.Shared.Magic.Components;
 using Content.Shared.Pointing;
@@ -24,10 +26,39 @@ public sealed partial class PetrificationSystem : EntitySystem
         SubscribeLocalEvent<PetrifiedComponent, PickupAttemptEvent>(OnAttempt);
         SubscribeLocalEvent<PetrifiedComponent, PointAttemptEvent>(OnAttempt);
         SubscribeLocalEvent<PetrifiedComponent, EmoteAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<PetrifiedComponent, CanSeeAttemptEvent>(OnAttempt);
+        SubscribeLocalEvent<PetrifiedComponent, IsEquippingAttemptEvent>(OnEquipAttempt);
+        SubscribeLocalEvent<PetrifiedComponent, IsUnequippingAttemptEvent>(OnUnequipAttempt);
     }
 
     private void OnAttempt(EntityUid uid, PetrifiedComponent comp, CancellableEntityEventArgs args)
     {
         args.Cancel();
+    }
+
+    private void OnEquipAttempt(EntityUid uid, PetrifiedComponent comp, IsEquippingAttemptEvent args)
+    {
+        if (args.Equipee == uid)
+            args.Cancel();
+    }
+
+    private void OnUnequipAttempt(EntityUid uid, PetrifiedComponent comp, IsUnequippingAttemptEvent args)
+    {
+        if (args.Unequipee == uid)
+            args.Cancel();
+    }
+
+    public bool TryPetrify(EntityUid uid)
+    {
+        EnsureComp<PetrifiedComponent>(uid);
+
+        return true;
+    }
+
+    public bool TryRemovePetrify(EntityUid uid)
+    {
+        var r = RemComp<PetrifiedComponent>(uid);
+
+        return r;
     }
 }
